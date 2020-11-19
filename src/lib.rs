01ca697e1c41c49f1b94931fb68171b75851174b
Justus K <justus.k@protonmail.com>
2020-11-19 19:10:53 +0100

    change implementation to use async / await

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,69 @@
+use async_fs as fs;
+use async_lock::RwLock;
+use blocking::unblock;
+use scrypt::{scrypt_check, scrypt_simple};
 use serde::{Deserialize, Serialize};
 use smol_str::SmolStr;
-use std::{io, path::Path};
+use std::{io, path::Path, sync::Arc};
 
-#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+#[derive(Default, Debug)]
 pub struct UserStore {
-    users: Vec<User>,
+    users: RwLock<Vec<User>>,
 }
 
 impl UserStore {
     /// Serializes this store and saves it to the given file.
-    pub fn save(&self, path: impl AsRef<Path>) -> io::Result<()> {
-        let bytes = bincode::serialize(self).expect("this should not fail to serialize");
-        std::fs::write(path, bytes)
+    pub async fn save(&mut self, path: impl AsRef<Path>) -> io::Result<()> {
+        let bytes =
+            bincode::serialize(self.users.get_mut()).expect("this should not fail to serialize");
+        fs::write(path, bytes).await
     }
 
     /// Reads bytes from the given file and deserializes it into a `UserStore`.
-    pub fn load(&self, path: impl AsRef<Path>) -> io::Result<Self> {
-        let bytes = std::fs::read(path)?;
-        let store: UserStore =
+    pub async fn load(path: impl AsRef<Path>) -> io::Result<Self> {
+        let bytes = fs::read(path).await?;
+        let store: Vec<User> =
             bincode::deserialize(&bytes).expect("someone edited the users file. aborting.");
-        Ok(store)
+        Ok(UserStore {
+            users: RwLock::new(store),
+        })
     }
 
     /// Inserts a new `User` into this store by hashing the raw password and the username
     /// that was given.
-    pub fn new_user(&mut self, name: impl Into<SmolStr>, raw_password: &str) {
-        let password = scrypt::scrypt_simple(raw_password, &scrypt::ScryptParams::recommended())
-            .expect("failed to generate random salt");
+    pub async fn new_user(self: &Arc<Self>, name: impl Into<SmolStr>, raw_password: String) {
+        let password = unblock(move || {
+            scrypt_simple(&raw_password, &scrypt::ScryptParams::recommended())
+                .expect("failed to generate random salt")
+        })
+        .await;
 
-        self.users.push(User {
+        self.users.write().await.push(User {
             name: name.into(),
             password,
         });
     }
 
     /// Removes a user, if one exists, with the given username.
-    pub fn delete_user(&mut self, name: impl Into<SmolStr>) {
+    pub async fn delete_user(self: &Arc<Self>, name: impl Into<SmolStr>) {
         let name = name.into();
-        self.users.retain(|user| user.name != name);
+        self.users.write().await.retain(|user| user.name != name);
     }
 
     /// Checks if a user with the given name exists, and verifies the password
     /// with the password stored for given the user.
-    pub fn check_user(&self, name: impl Into<SmolStr>, password: &str) -> bool {
+    pub async fn check_user(self: &Arc<Self>, name: impl Into<SmolStr>, password: String) -> bool {
         let name = name.into();
 
-        if let Some(user) = self.users.iter().find(|user| user.name == name) {
-            scrypt::scrypt_check(password, user.password.as_ref()).is_ok()
+        if let Some(user) = self
+            .users
+            .read()
+            .await
+            .iter()
+            .find(|user| user.name == name)
+        {
+            let user_password = user.password.clone();
+            unblock(move || scrypt_check(&password, &user_password).is_ok()).await
         } else {
             false
         }
@@ -67,14 +84,18 @@ mod tests {
 
     #[test]
     fn test_user_store() {
-        let mut store = UserStore::default();
-        store.new_user("stu", "hello123");
+        async fn inner() {
+            let store = Arc::new(UserStore::default());
+            store.new_user("stu", "hello123".into()).await;
 
-        assert!(store.check_user("stu", "hello123"));
-        assert!(!store.check_user("stu", "invalid"));
+            assert!(store.check_user("stu", "hello123".into()).await);
+            assert!(!store.check_user("stu", "invalid".into()).await);
 
-        store.delete_user("stu");
+            store.delete_user("stu").await;
 
-        assert!(!store.check_user("stu", "hello123"));
+            assert!(!store.check_user("stu", "hello123".into()).await);
+        }
+
+        smol::block_on(inner());
     }
 }
